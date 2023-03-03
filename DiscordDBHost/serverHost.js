666c897a6f3562c1e5f75a8e5ba484cc6be356fd
root <mcorreale1@gmail.com>
2023-03-03 03:09:43 +0000

    Writing to db on get w/ params

--- DiscordDBHost/serverHost.js
+++ DiscordDBHost/serverHost.js
@@ -6,19 +6,32 @@ const path = require('path');
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const mongoURI = fs.readFileSync('./conf.txt', 'utf-8').toString();
-
 const mdclient = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 mdclient.connect();
 
-app.get('/', function(req, res) {
-	
+app.get('/', async function(req, res) {
+    db =  mdclient.db("nyFutureBot");
+    col = db.collection("lbRequests");
+    const doc = {
+            user:req.query.user,
+            createdDate: new Date()
+    };
+
+    const result = await col.insertOne(doc);
+
+
+    console.log(result);
+    res.send(result);
+
+           
+
 });
 
 
 
 
-const server = http.listen(8080, function() {
-    dbList =  mdclient.db().admin().listDatabases();
-    console.log(dbList);
+const server = app.listen(8080, function() {
+    
     console.log('listening on *:8080');
 });
+
